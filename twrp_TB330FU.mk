eb12a39f1b8ee6188ef780bbdf4f82a99dd11089
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-06-07 21:19:21 +0100

    Update and rename omni_TB330FU.mk to twrp_TB330FU.mk

--- twrp_TB330FU.mk
+++ twrp_TB330FU.mk
@@ -7,23 +7,21 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TB330FU device
 $(call inherit-product, device/lenovo/TB330FU/device.mk)
 
 PRODUCT_DEVICE := TB330FU
-PRODUCT_NAME := omni_TB330FU
+PRODUCT_NAME := twrp_TB330FU
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := TB330FU
 PRODUCT_MANUFACTURER := lenovo
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_barley_row_wifi-user 12 SP1A.210812.016 18 release-keys"
-
-BUILD_FINGERPRINT := Lenovo/TB330FU/TB330FU:12/SP1A.210812.016/_231020_ROW:user/release-keys
